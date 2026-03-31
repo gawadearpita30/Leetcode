e36d34fea35fbd8265d10a478f81ca95e1f704a7
gawadearpita30 <gawadearpita30@gmail.com>
2026-03-31 17:10:45 +0530

    Time: 2 ms (85.80%) | Memory: 48 MB (52.37%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i > max)
+            {
+                return false;
+            }
+
+            if(i + nums[i] > max)
+            {
+                max = i + nums[i];
+            }
+        }
+        return true;
+    }
+}
