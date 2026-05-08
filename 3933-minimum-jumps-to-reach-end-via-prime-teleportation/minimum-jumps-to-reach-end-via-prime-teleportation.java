e43343329d18c144618a1aaf5cb0983b83c34291
gawadearpita30 <gawadearpita30@gmail.com>
2026-05-08 18:28:59 +0530

    Time: 706 ms (14.16%) | Memory: 179.9 MB (92.69%) - LeetSync

---
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+class Solution {
+
+    public int minJumps(int[] nums) {
+        int n = nums.length;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int p : getPrimeFactors(nums[i])) {
+                map.computeIfAbsent(p, k -> new ArrayList<>()).add(i);
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+
+        q.offer(0);
+        vis[0] = true;
+
+        int steps = 0;
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            while (size-- > 0) {
+
+                int i = q.poll();
+
+                if (i == n - 1) return steps;
+
+                if (i - 1 >= 0 && !vis[i - 1]) {
+                    vis[i - 1] = true;
+                    q.offer(i - 1);
+                }
+
+                if (i + 1 < n && !vis[i + 1]) {
+                    vis[i + 1] = true;
+                    q.offer(i + 1);
+                }
+
+                if (isPrime(nums[i])) {
+
+                    int p = nums[i];
+
+                    if (map.containsKey(p)) {
+
+                        for (int idx : map.get(p)) {
+
+                            if (!vis[idx]) {
+                                vis[idx] = true;
+                                q.offer(idx);
+                            }
+                        }
+
+                        map.remove(p);
+                    }
+                }
+            }
+
+            steps++;
+        }
+
+        return -1;
+    }
+
+    private boolean isPrime(int x) {
+
+        if (x < 2) return false;
+
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    private Set<Integer> getPrimeFactors(int x) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 2; i * i <= x; i++) {
+
+            if (x % i == 0) {
+
+                set.add(i);
+
+                while (x % i == 0) {
+                    x /= i;
+                }
+            }
+        }
+
+        if (x > 1) set.add(x);
+
+        return set;
+    }
+}
